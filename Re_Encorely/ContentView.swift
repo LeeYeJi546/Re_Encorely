e0812a0b061cbe24ca806d34a988404eb010f71d
leeyeji <yeji1135@gachon.ac.kr>
2025-11-16 16:21:34 +0900

    first commit

---
+++ Re_Encorely/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Re_Encorely
+//
+//  Created by 이예지 on 11/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
